cf19b14ab9e8c9ea9557024872c358b11c7ef02b
Thomas Grinderslev <thomas@edulab.dk>
2019-01-24 19:05:57 +0100

    Added a basic pizza list

---
+++ components/Ingredient.vue
@@ -0,0 +1,35 @@
+<template>
+  <li class="ingredient">{{ingredient}}</li>
+</template>
+
+<script>
+  export default {
+    props: {
+      ingredient: {
+        type: String,
+        default: undefined,
+      },
+    },
+  }
+</script>
+
+<style lang="less">
+  .ingredient {
+    display: inline-block;
+    padding: 0 .4em;
+    margin-left: -.2em;
+    transition: background .2s ease-out;
+
+    &:first-child {
+      margin-left: -.4em;
+    }
+
+    &:not(:last-child):after {
+      content: ', ';
+    }
+
+    &:hover {
+      background: #BBE2A0;
+    }
+  }
+</style>
---
+++ pages/pizzas.vue
@@ -0,0 +1,61 @@
+<template>
+  <article>
+    <h1>List of pizzas</h1>
+    <ol>
+      <li
+        v-for="pizza in pizzas"
+        :key="pizza.name"
+      >
+        {{pizza.name}}
+        <br>
+        <ul class="ingredients">
+          <Ingredient
+            v-for="ingredient in pizza.ingredients"
+            :key="ingredient"
+            :ingredient="ingredient"
+          />
+        </ul>
+      </li>
+    </ol>
+  </article>
+</template>
+
+<script>
+  import Ingredient from '~/components/Ingredient'
+
+  export default {
+    components: { Ingredient },
+    data() {
+      return {
+        pizzas: [
+          {
+            name: 'Margherita',
+            ingredients: [
+              'Tomato sauce',
+              'Mozzarella cheese',
+              'Fresh basil',
+              'Olive oil',
+            ],
+          },
+          {
+            name: 'Marinara',
+            ingredients: [
+              'Tomato sauce',
+              'Oregano',
+              'Garlic',
+            ],
+          },
+        ],
+      }
+    },
+    methods: {
+    },
+  }
+</script>
+
+<style>
+  .ingredients {
+    font-size: .8em;
+    padding-left: 0;
+  }
+</style>
